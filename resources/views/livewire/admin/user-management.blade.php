130cac9e1c2b6ecf72522deae5394d65c4eaf31f
Kojie <quilatonlyn449@gmail.com>
2024-02-20 10:19:21 +0800

    add a design

--- resources/views/livewire/admin/user-management.blade.php
+++ resources/views/livewire/admin/user-management.blade.php
@@ -40,9 +40,9 @@
                     });
                 </script>
             @endif
-            <div class="card">
+            <div class="card card-warning">
                 <div class="card-header">
-                    <h3 class="card-title">Approve User ({{ $totalUser }})</h3>
+                    <h3 class="card-title">Total User: <span style="font-weight:bold">{{ $totalUser }}</span></h3>
                 </div>
                 <div class="card-body p-0">
                     <div class="table-responsive">
